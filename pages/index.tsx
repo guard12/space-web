033807b46d938d57a0965dd3f8ae7e2d6e8784ed
Matus Kollarcik <mko@coinify.com>
2022-08-02 15:57:10 +0200

    Change font

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,12 +12,12 @@ const Home: NextPage = () => {
 
       <div className="relative flex items-center justify-center h-screen overflow-hidden">
         <div className="absolute z-30 p-5">
-          <div className="text-4xl font-bold">
-            DO YOU SEE OUR FUTURE IN SPACE?
-          </div>
-          <div className="text-xl font-normal text-center">
-            Then you are at the right place.
-          </div>
+          <h1 className="text-4xl font-bold uppercase">
+            Looking for a space startup?
+          </h1>
+          <h2 className="text-3xl font-light text-center">
+            So was I.
+          </h2>
         </div>
         <Image
           className="z-10 w-auto min-w-full min-h-full max-w-none opacity-60"
@@ -26,11 +26,19 @@ const Home: NextPage = () => {
           alt="mars-background"
         />
       </div>
-      <div className="p-10 w-1/2">
-        <div className="text-base font-normal text-left">
+      <div className="flex justify-center pb-8 px-40">
+        <div className="text-base font-normal text-center">
           Were you endlessly browsing the web in the belief that you will find a job in a space startup?
-          So was I, unfortunately I have found exactly 0 startups in Copenhagen which would push the humanity
-          further.
+          So was I, unfortunately I have found exactly 0 startups in Copenhagen, this made me sad. So I have 
+          decided to find people who think like me so together we can help companies like SpaceX to expand human 
+          achievements in space.
+        </div>
+      </div>
+      <div className="flex justify-center pb-3 px-40">
+        <div className="text-base font-normal text-center">
+            At XXX we believe that space is and should be for everyone. Therefore we are joining companies like 
+            spaceX, Relativity Space or NASA/ESA on the mission to explore and unfold the universe. If you think you 
+            know how to help and have a strong desire to help, let us know!
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+      'sans': ['Lato', 'Helvetica', 'Arial', 'sans-serif'],
+    },
     extend: {}
   },
   plugins: [],
